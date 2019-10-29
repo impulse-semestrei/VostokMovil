5aacec56d9fa7051bc0a446b7119846f6614e2c7
Andres Quiroz <andresq1595@gmail.com>
2019-10-29 09:33:47 -0600

    FE

--- Vostok/TextBox.js
+++ Vostok/TextBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+import { TextInput,StyleSheet, View, Text } from 'react-native';
 
 export default class TextBox extends React.Component {
   constructor(props){
@@ -12,8 +12,33 @@ export default class TextBox extends React.Component {
   }
 
   render(){
+    if (parseInt(this.state.text)< this.props.objetivo){
+      return(
+        <View style={styles.container}>
+
+          <Text style={styles.label}>Cantidad: </Text>
+            <TextInput
+              style={styles.textInputWrong}
+              keyboardType='numeric'
+              onChangeText={
+                text => {
+                  this.setState({text: text})
+                  this.props.onChange(text)
+                }
+              }
+              value={this.state.text}
+            />
+          </View>
+      )
+
+    }else{
+
+
     return(
+      <View style={styles.container}>
+        <Text style={styles.label}>Cantidad: </Text>
           <TextInput
+            style={styles.textInput}
             keyboardType='numeric'
             onChangeText={
               text => {
@@ -23,7 +48,43 @@ export default class TextBox extends React.Component {
             }
             value={this.state.text}
           />
+        </View>
+
     )
+        }
+
   }
 
 }
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+    marginHorizontal: 140
+  },
+  label: {
+    color: '#555555',
+    width: 100
+  },
+  textInput: {
+    width : 60,
+    height : 60,
+    fontSize: 40,
+    textAlign:'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#27bb30',
+    backgroundColor: '#ccefc2',
+  },
+  textInputWrong :{
+    width : 60,
+    height : 60,
+    fontSize: 40,
+    textAlign:'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ac5450',
+    backgroundColor: '#efc2c2'
+  }
+
+
+});
