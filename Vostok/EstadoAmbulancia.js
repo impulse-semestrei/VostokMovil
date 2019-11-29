2b7f9f0df5efdae88fd3b69e0bbe5de121b5fe2a
tecmanny <A01207405@itesm.mx>
2019-11-29 15:00:19 -0600

    URLs

--- Vostok/EstadoAmbulancia.js
+++ Vostok/EstadoAmbulancia.js
@@ -10,7 +10,7 @@ export default class EstadoAmbulancia extends React.Component {
   render(){
     return(
       <Checklist
-        url={'http://10.0.2.2:8000/ambulancia/'+this.props.navigation.getParam('id_ambulancia')+'/json/'}
+        url={'http://rescate1app.com/ambulancia/'+this.props.navigation.getParam('id_ambulancia')+'/json/'}
         nombre_paramedico={this.props.navigation.getParam('nombre_paramedico')}
         email_paramedico={this.props.navigation.getParam('email_paramedico')}
         onBack={() => this.props.navigation.dispatch(
@@ -20,7 +20,7 @@ export default class EstadoAmbulancia extends React.Component {
               actions: [
                 NavigationActions.navigate({routeName: 'First'}),
                 NavigationActions.navigate({
-                  routeName: 'Ambulancia', 
+                  routeName: 'Ambulancia',
                   params: {
                     NameOBJ: this.props.navigation.getParam('nombre_paramedico'),
                     EmailOBJ: this.props.navigation.getParam('email_paramedico'),
